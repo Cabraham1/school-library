f90b409c7319818ad64a0d3ab7e0d61c39ecb204
cabraham1 <christopherabraham8@gmail.com>
2023-05-19 17:00:13 +0100

    Add main file

--- main.rb
+++ main.rb
@@ -1,14 +1,19 @@
-puts 'Welcome to School Library App!'
-puts
-def display_list
-  puts 'Welcome to School library App!'
-  puts "
-      Choose an option by entering a number:
-      1 - List all books
-      2 - List all people
-      3 - Create a person
-      4 - Create a book
-      5 - Create a rental
-      6 - List all rentals for a given person id
-      7 - Exit"
-end
+require_relative './app'
+class Main 
+  def self.home_page
+    puts 'Welcome to School library App!'
+    puts 'Please enter a number: '
+
+    @content = {
+      '1' => 'List all books',
+      '2' => 'List all people',
+      '3' => 'Create a person (teacher or student)',
+      '4' => 'Create a book',
+      '5' => 'Create a rental',
+      '6' => 'List all rentals for a given person id',
+      '7' => 'Exit'
+    }
+    @content.each { |index, string| puts "#{index} - #{string}" }
+
+    Integer(gets.chomp)
+  end
