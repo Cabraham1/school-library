67417e01393d95d9a108416a8c3bce35f8c240b6
cabraham1 <christopherabraham8@gmail.com>
2023-05-19 17:01:29 +0100

    main options

--- main.rb
+++ main.rb
@@ -17,3 +17,34 @@ class Main
 
     Integer(gets.chomp)
   end
+
+  app = App.new
+
+  loop do
+    case home_page
+    when 1
+      app.book_list
+    when 2
+      app.person_list
+    when 3
+      app.create_person
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      app.rental_list
+    when 7
+      puts 'You made a good choice'
+      exit
+    else
+      puts 'You are out of range'
+    end
+  end
+end
+
+def main
+  App.new
+end
+
+Main
