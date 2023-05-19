4ccdbbe8de1748ea050e8d7384c48100f057cc30
cabraham1 <christopherabraham8@gmail.com>
2023-05-19 16:44:12 +0100

    Import required files

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
-def shout
-  '-------'
-end
+require_relative './decorator'
+require_relative './trimmer_decorator'
+require_relative './capitalize_decorator'
+require_relative './person'
+require_relative './student'
+require_relative './teacher'
+require_relative './classroom'
+require_relative './rental'
+require_relative './book'
+
